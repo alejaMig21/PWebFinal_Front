a8f6fdd0fa3af3bf64a84c1f1429392cb9738697
Alejandro <alejasainzmartinez@gmail.com>
2022-12-18 16:34:02 +0100

    [CRUDs iniciales]+[Delegados]+[LanguageBean]+[Electoral Process]+[Users,Roles]+[Frindly URL]

--- pweb-crud-demo/src/main/java/cu/edu/cujae/pweb/config/UrlRewriteConfigurationProvider.java
+++ pweb-crud-demo/src/main/java/cu/edu/cujae/pweb/config/UrlRewriteConfigurationProvider.java
@@ -15,8 +15,16 @@ public class UrlRewriteConfigurationProvider extends HttpConfigurationProvider {
     public Configuration getConfiguration(ServletContext context) {
         return ConfigurationBuilder.begin()
 
-                .addRule(Join.path("/security-users").to("/pages/security/users/user-list.jsf"))
-                .addRule(Join.path("/welcome").to("/pages/welcome/welcome.jsf"));
+                .addRule(Join.path("/security-users").to("/pages/security/users/lista_user.jsf"))
+                .addRule(Join.path("/security-roles").to("/pages/security/users/lista_roles.jsf"))
+                .addRule(Join.path("/welcome").to("/pages/welcome/welcome.jsf"))
+                .addRule(Join.path("/lista_municipality").to("/pages/menu/lista_municipality.jsf"))
+                .addRule(Join.path("/lista_district").to("/pages/menu/lista_district.jsf"))
+                .addRule(Join.path("/lista_college").to("/pages/menu/lista_college.jsf"))
+                .addRule(Join.path("/lista_cdr").to("/pages/menu/lista_cdr.jsf"))
+                .addRule(Join.path("/lista_voter").to("/pages/menu/lista_votantes.jsf"))
+                .addRule(Join.path("/lista_nominated").to("/pages/menu/lista_nominados.jsf"))
+                .addRule(Join.path("/lista_delegated").to("/pages/menu/lista_delegated.jsf"));
 
 
         //ej using params
